8c05512c2c72670740edf5166bc106d1e36ae677
Guilherme Fonseca <guilhermelimafonseca@gmail.com>
2024-12-08 00:06:43 -0300

    Refactor index.ts: standardize import statements and improve code formatting for consistency

--- index.ts
+++ index.ts
@@ -1,31 +1,29 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 
-import avaliacaoRoutes from './src/routes/avaliacao'
-import clientesRoutes from './src/routes/clientes'
-import prestadorRoutes from './src/routes/prestador'
-import reservaRoutes from './src/routes/reserva'
-import propostasRoutes from './src/routes/servico'
-import usuarioRoutes from './src/routes/usuario'
+import avaliacaoRoutes from "./src/routes/avaliacao";
+import clientesRoutes from "./src/routes/clientes";
+import prestadorRoutes from "./src/routes/prestador";
+import reservaRoutes from "./src/routes/reserva";
+import propostasRoutes from "./src/routes/servico";
 
-const app = express()
-const port = 3004
+const app = express();
+const port = 3004;
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
 
-app.use("/avaliacao", avaliacaoRoutes)
-app.use("/clientes", clientesRoutes)
-app.use("/prestador", prestadorRoutes)
-app.use("/reserva", reservaRoutes)
-app.use("/propostas", propostasRoutes)
-app.use("/usuarios", usuarioRoutes)
+app.use("/avaliacao", avaliacaoRoutes);
+app.use("/clientes", clientesRoutes);
+app.use("/prestador", prestadorRoutes);
+app.use("/reserva", reservaRoutes);
+app.use("/propostas", propostasRoutes);
 
-app.get('/', (req, res) => {
-  res.send('API: Sistema Mão na Roda')
-})
+app.get("/", (req, res) => {
+  res.send("API: Sistema Mão na Roda");
+});
 
 app.listen(port, () => {
-  console.log(`Servidor rodando na porta: ${port}`)
-})
+  console.log(`Servidor rodando na porta: ${port}`);
+});
