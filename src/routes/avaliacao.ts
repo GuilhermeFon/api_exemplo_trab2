0320bb467842dcd8445fe8857c57f2c2b3db07a7
Guilherme Fonseca <guilhermelimafonseca@gmail.com>
2024-12-07 23:58:26 -0300

    Refactor avaliacao routes: consolidate endpoints for cleaner structure and improved readability

--- src/routes/avaliacao.ts
+++ src/routes/avaliacao.ts
@@ -1,49 +1,14 @@
-import {Router} from "express";
-import * as avaliacaoController from "../controllers/avaliacaoController";
-import {authMiddleware} from "../middlewares/authMiddleware";
+import { Router } from 'express';
+import * as avaliacaoController from '../controllers/avaliacaoController';
+import { authMiddleware } from '../middlewares/authMiddleware';
 
 const router = Router();
 
-router.post(
-  "/avaliacao/prestador",
-  authMiddleware,
-  avaliacaoController.createAvaliacao
-);
-router.post(
-  "/avaliacao/cliente",
-  authMiddleware,
-  avaliacaoController.createAvaliacao
-);
-router.get("/avaliacao/prestador/:id", avaliacaoController.getAvaliacao);
-router.get("/avaliacao/cliente/:id", avaliacaoController.getAvaliacao);
-router.get(
-  "/prestador/:prestadorId",
-  avaliacaoController.getAvaliacoesPorPrestador
-);
-router.get(
-  "/cliente/:clienteId",
-  authMiddleware,
-  avaliacaoController.getAvaliacoesPorCliente
-);
-router.put(
-  "/avaliacao/prestador/:id",
-  authMiddleware,
-  avaliacaoController.updateAvaliacao
-);
-router.put(
-  "/avaliacao/cliente/:id",
-  authMiddleware,
-  avaliacaoController.updateAvaliacao
-);
-router.delete(
-  "/avaliacao/prestador/:id",
-  authMiddleware,
-  avaliacaoController.deleteAvaliacao
-);
-router.delete(
-  "/avaliacao/cliente/:id",
-  authMiddleware,
-  avaliacaoController.deleteAvaliacao
-);
+router.post('/', authMiddleware, avaliacaoController.createAvaliacao);
+router.get('/:id', avaliacaoController.getAvaliacao);
+router.get('/prestador/:prestadorId', avaliacaoController.getAvaliacoesPorPrestador);
+router.get('/cliente/:clienteId', authMiddleware, avaliacaoController.getAvaliacoesPorCliente);
+router.put('/:id', authMiddleware, avaliacaoController.updateAvaliacao);
+router.delete('/:id', authMiddleware, avaliacaoController.deleteAvaliacao);
 
 export default router;
